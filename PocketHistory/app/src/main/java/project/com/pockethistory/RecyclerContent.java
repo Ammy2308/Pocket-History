6325f2aa9cad72db2cee65c687e7d26ae15ceac2
Vishal <vishal.sharma@kony.com>
2015-09-05 16:39:14 +0530

    A very few changes

--- PocketHistory/app/src/main/java/project/com/pockethistory/RecyclerContent.java
+++ PocketHistory/app/src/main/java/project/com/pockethistory/RecyclerContent.java
@@ -1,7 +1,4 @@
 package project.com.pockethistory;
 
-/**
- * Created by KH2021 on 9/5/2015.
- */
 public class RecyclerContent {
 }
--- PocketHistory/app/src/main/java/project/com/pockethistory/Utils.java
+++ PocketHistory/app/src/main/java/project/com/pockethistory/Utils.java
@@ -1,18 +1,5 @@
 package project.com.pockethistory;
 
-import android.content.Context;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.afollestad.materialdialogs.MaterialDialog;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-/**
- * Created by KH2021 on 7/12/2015.
- */
 public class Utils {
-
+    public static final String BASE_URL = "http://api.todevs.com/history/";
 }
