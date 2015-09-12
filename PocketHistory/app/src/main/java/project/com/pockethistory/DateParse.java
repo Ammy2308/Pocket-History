bb4e5c8b49337f4108f186f055d576611c1419a6
Vishal <vishal.sharma@kony.com>
2015-09-12 20:12:38 +0530

    Setting recycler content

--- PocketHistory/app/src/main/java/project/com/pockethistory/DateParse.java
+++ PocketHistory/app/src/main/java/project/com/pockethistory/DateParse.java
@@ -14,6 +14,7 @@ public class DateParse {
 
     public List<Object> dateParseImplementation(String jsonString) throws JSONException {
         JSONObject dataObject = new JSONObject(jsonString).getJSONObject("data");
+        String part_heading = dataObject.getString("date");
         Iterator<?> keys = dataObject.keys();
         List<JSONObject> returnableData = new ArrayList<>();
         while(keys.hasNext()) {
@@ -23,12 +24,13 @@ public class DateParse {
             JSONArray dataArray = dataObject.getJSONArray(key);
             for (int i = 0; i < dataArray.length(); i++) {
                 RecyclerContent newContent = new RecyclerContent();
-
+                JSONObject obj = dataArray.getJSONObject(i);
+                String heading = part_heading + obj.getString("year");
+                Log.e("yo", obj.toString());
             }
 
             Log.e("yo", dataArray.getClass().getName());
         }
-//        returnableData.add(keyTerms);
         return null;
     }
 }
--- PocketHistory/app/src/main/java/project/com/pockethistory/RecyclerContent.java
+++ PocketHistory/app/src/main/java/project/com/pockethistory/RecyclerContent.java
@@ -1,4 +1,19 @@
 package project.com.pockethistory;
 
 public class RecyclerContent {
+    private String heading;
+    private String content;
+
+    public void setPaletteContent(String heading, String content) {
+        this.heading = heading;
+        this.content = content;
+    }
+
+    public String getPaletteHeading() {
+        return heading;
+    }
+
+    public String getPaletteContent() {
+        return content;
+    }
 }
